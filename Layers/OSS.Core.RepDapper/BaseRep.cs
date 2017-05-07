94421af83305cec98c506694a0ecd6d8ba16476e
Kevin <1985088337@qq.com>
2017-05-07 16:29:24 +0800

    完成底层基础读写分离封装

--- Layers/OSS.Core.RepDapper/BaseRep.cs
+++ Layers/OSS.Core.RepDapper/BaseRep.cs
@@ -11,6 +11,12 @@
 
 #endregion
 
+using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using OSS.Common.ComModels;
+using OSS.Common.ComModels.Enums;
+using OSS.Common.Modules.LogModule;
 using OSS.Core.DomainMos;
 
 namespace OSS.Core.RepDapper
@@ -23,8 +29,73 @@ namespace OSS.Core.RepDapper
     {
         protected  string m_TableName;
 
-        //  插入，判断是否返回id,   todo type全称，是否自增长，连接串 缓存
-        //  todo  如果是枚举值和布尔值，需要替换成数字   
+
+        protected readonly string writeConnectionString;
+        protected readonly string readeConnectionString;
+
+        public BaseRep(string writeConnectionStr=null,string readeConnectionStr=null)
+        {
+            writeConnectionString = writeConnectionStr;
+            readeConnectionString = readeConnectionStr;
+        }
+
+        //public virtual ResultIdMo Insert(TType mo)
+        //{
+
+        //}
+
+
+        #region 底层基础读写分离封装
+        /// <summary>
+        /// 执行写数据库操作
+        /// </summary>
+        /// <typeparam name="RType"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        protected internal RType ExcuteWrite<RType>(Func<IDbConnection, RType> func) where RType : ResultMo, new()
+            => Execute(func, true);
+   
+        /// <summary>
+        ///  执行读数据库操作
+        /// </summary>
+        /// <typeparam name="RType"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        protected internal ResultMo<RType> ExcuteReade<RType>(Func<IDbConnection, RType> func) => Execute(con =>
+        {
+            var res = func(con);
+            return res != null ? new ResultMo<RType>(res) : new ResultMo<RType>(ResultTypes.ObjectNull, "未发现相关数据！");
+        }, false);
+
+        private  RType Execute<RType>(Func<IDbConnection, RType> func,bool isWrite)
+            where RType : ResultMo,new ()
+        {
+            var t = default(RType);
+            try
+            {
+                using (var con=new MySqlConnection(isWrite? writeConnectionString: readeConnectionString))
+                {
+                    t= func(con);
+                }
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                throw e;
+#else
+                 LogUtil.Error(string.Concat("数据库操作错误，详情：", e.Message, "\r\n", e.StackTrace), "DataRepConnectionError",
+                    "DapperRep");
+                t = new RType
+                {
+                    Ret = (int) ResultTypes.InnerError,
+                    Message = "数据更新出错！"
+                };
+                return t;
+#endif
+            }
+            return t ?? new RType() {Ret =(int)ResultTypes.ObjectNull,Message = "未发现对应结果响应"};
+        }
+        #endregion
 
     }
 
