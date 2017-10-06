faa4f110b551eae2316e1e2d446f291efd25c566
KevinWG <1985088337@qq.com>
2017-10-06 16:38:32 +0800

    添加404页面

---
+++ FrontEnds/OSS.Core.WebSite/Controllers/OauthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OSS.Core.Infrastructure.Utils;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OSS.Core.WebSite.Controllers
+{
+    public class OauthController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult wx()
+        //{
+            //var urlRes=ApiUtil.PostCoreApi<>()
+        //}
+    }
+}
