109f8687def43a236972c1559dc1bebb205a594d
KevinWG <1985088337@qq.com>
2017-10-06 16:56:51 +0800

    添加第三方平台枚举

---
+++ Infrastructure/OSS.Core.Infrastructure/Enums/ThirdPaltforms.cs
@@ -0,0 +1,43 @@
+﻿
+#region Copyright (C) 2017 Kevin (OSS开源作坊) 公众号：osscoder
+
+/***************************************************************************
+*　　	文件功能描述：OSSCore —— 第三方平台名称
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-10-6
+*       
+*****************************************************************************/
+
+#endregion
+
+namespace OSS.Core.Infrastructure.Enums
+{
+    /// <summary>
+    ///  第三方平台
+    /// </summary>
+    public enum ThirdPaltforms
+    {
+        /// <summary>
+        ///  系统本身
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// 微信
+        /// </summary>
+        Wechat = 10,
+
+        /// <summary>
+        ///  支付宝
+        /// </summary>
+        AliPay = 20,
+
+        /// <summary>
+        ///  新浪
+        /// </summary>
+        Sina=30
+            
+    }
+}
