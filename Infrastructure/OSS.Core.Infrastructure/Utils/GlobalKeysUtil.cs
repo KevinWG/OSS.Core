2baa393ec151475790b39bdfb34c5c5773e7448e
KevinWG <1985088337@qq.com>
2017-10-21 23:46:07 +0800

    添加注释

--- Infrastructure/OSS.Core.Infrastructure/Utils/GlobalKeysUtil.cs
+++ Infrastructure/OSS.Core.Infrastructure/Utils/GlobalKeysUtil.cs
@@ -1,5 +1,21 @@
-﻿namespace OSS.Core.Infrastructure.Utils
+﻿#region Copyright (C) 2017 Kevin (OSS开源作坊) 公众号：osscoder
+
+/***************************************************************************
+*　　	文件功能描述：OSSCore ——  系统关键Key
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-5-16
+*       
+*****************************************************************************/
+
+#endregion
+
+namespace OSS.Core.Infrastructure.Utils
 {
+    /// <summary>
+    /// 系统关键Key
+    /// </summary>
     public class GlobalKeysUtil
     {
         #region  cookie等名称
