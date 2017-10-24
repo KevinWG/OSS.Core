d8fa61d0ccc2e5aeee31ecf1562a46646a9e4e4b
KevinWG <1985088337@qq.com>
2017-10-25 00:20:33 +0800

    更新表语句

--- oss_core.sql
+++ oss_core.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : test
+Source Server         : osscore_test
 Source Server Version : 50718
 Source Host           : 13.67.112.156:3306
 Source Database       : oss_core
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2017-09-18 13:42:29
+Date: 2017-10-25 00:19:42
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,23 +29,26 @@ CREATE TABLE `admin_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for third_platform_users
+-- Table structure for oauth_user_info
 -- ----------------------------
-DROP TABLE IF EXISTS `third_platform_users`;
-CREATE TABLE `third_platform_users` (
-  `Id` bigint(8) DEFAULT NULL,
+DROP TABLE IF EXISTS `oauth_user_info`;
+CREATE TABLE `oauth_user_info` (
+  `Id` bigint(8) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(8) DEFAULT NULL COMMENT '用户Id',
-  `platform_source` int(4) DEFAULT NULL COMMENT '第三方平台来源类型',
-  `union_id` varchar(255) DEFAULT NULL COMMENT '第三方平台下的多应用唯一用户Id',
-  `open_id` varchar(255) DEFAULT NULL COMMENT '第三方平台下单应用用户Id',
-  `sex` tinyint(2) DEFAULT NULL COMMENT '性别',
-  `avatar` varchar(500) DEFAULT NULL COMMENT '头像',
-  `app_source` varchar(255) DEFAULT NULL COMMENT '应用来源',
   `tenant_id` varchar(255) DEFAULT NULL COMMENT '租户Id',
+  `app_source` varchar(255) DEFAULT NULL COMMENT '应用来源',
+  `status` tinyint(2) DEFAULT NULL COMMENT '当前状态',
+  `app_user_id` varchar(20) DEFAULT NULL,
+  `app_union_id` varchar(255) DEFAULT NULL COMMENT '第三方平台下的多应用唯一用户Id',
   `access_token` varchar(255) DEFAULT NULL COMMENT '授权AccessToken',
   `refresh_token` varchar(255) DEFAULT NULL COMMENT '刷新Token信息',
+  `expire_date` bigint(8) DEFAULT NULL,
+  `platform` int(4) DEFAULT NULL COMMENT '第三方平台来源类型',
+  `nick_name` varchar(255) DEFAULT NULL,
+  `sex` tinyint(2) DEFAULT NULL COMMENT '性别',
+  `head_img` varchar(500) DEFAULT NULL COMMENT '头像',
   `create_time` bigint(8) DEFAULT NULL COMMENT '创建时间戳',
-  `status` tinyint(2) DEFAULT NULL COMMENT '当前状态'
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
@@ -57,10 +60,12 @@ CREATE TABLE `user_info` (
   `mobile` varchar(100) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `pass_word` varchar(64) DEFAULT NULL,
+  `tenant_id` bigint(8) DEFAULT NULL,
+  `status` int(4) DEFAULT NULL,
   `nick_name` varchar(255) DEFAULT NULL COMMENT '昵称',
+  `head_img` varchar(255) DEFAULT NULL,
   `app_source` varchar(300) DEFAULT NULL,
   `app_version` varchar(50) DEFAULT NULL,
-  `status` int(4) DEFAULT NULL,
   `create_time` bigint(8) DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
