711d71b2e4bb654a5ad9f89117ec6f75a9eb92cc
KevinWG <1985088337@qq.com>
2017-11-01 20:20:39 +0800

    调整目录结构

---
+++ OSS.ServiceApis/OSS.Core.RepDapper/Members/AdminInfoRep.cs
@@ -0,0 +1,27 @@
+﻿#region Copyright (C) 2017 Kevin (OSS开源作坊) 公众号：osscoder
+
+/***************************************************************************
+*　　	文件功能描述：OSSCore —— 后台管理员仓储实现
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-5-17
+*       
+*****************************************************************************/
+
+#endregion
+
+
+namespace OSS.Core.RepDapper.Members
+{
+    /// <summary>
+    ///  后台管理员仓储实现
+    /// </summary>
+    public class AdminInfoRep: BaseRep<AdminInfoRep>
+    {
+        public AdminInfoRep()
+        {
+            m_TableName = "admin_info";
+        }
+    }
+}
---
+++ OSS.ServiceApis/OSS.Core.WebApi/Startup.cs
@@ -0,0 +1,130 @@
+﻿#region Copyright (C) 2017 Kevin (OSS开源作坊) 公众号：osscoder
+
+/***************************************************************************
+*　　	文件功能描述：OSSCore —— 程序启动配置类
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-5-16
+*       
+*****************************************************************************/
+
+#endregion
+
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using OSS.Common.ComUtils;
+using OSS.Core.Infrastructure.Plugs;
+using OSS.Core.Infrastructure.Utils;
+using OSS.Core.Services.Sns.Exchange;
+using OSS.Core.WebApi.Filters;
+using OSS.Plugs.TemplateMsg.Email;
+using OSS.Plugs.TemplateMsg.Sms;
+
+namespace OSS.Core.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            ConfigUtil.Configuration = Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(mvcOp => mvcOp.Filters.Add(new AuthorizeSignAttribute())).AddJsonOptions(op =>
+            {
+                //  设置属性序列化规范
+                op.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                // 去除序列化中的默认值和空值
+                op.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                op.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
+            });
+
+            RegisteGlobal();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            ConfigStaticFiles(app);
+            app.UseExceptionMiddleware(); // 注册全局错误
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}");
+
+                //routes.MapRoute(
+                //    name: "coreapi",
+                //    template: "core/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        /// <summary>
+        ///   处理默认和静态文件
+        /// </summary>
+        /// <param name="app"></param>
+        private static void ConfigStaticFiles(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory())),
+                DefaultContentType = "image/x-icon"
+            });
+        }
+        
+        /// <summary>
+        /// 注册全局配置等相关实现
+        /// </summary>
+        private static void RegisteGlobal()
+        {
+            RegistePlugs();
+            RegisterWxConfig();
+        }
+        
+    
+
+        /// <summary>
+        ///  注册微信配置信息
+        /// </summary>
+        private static void RegisterWxConfig()
+        {
+            var appId = ConfigUtil.GetSection("DefaultWxConfig:AppId")?.Value;
+            var appSecret = ConfigUtil.GetSection("DefaultWxConfig:AppSecret")?.Value;
+            if (!string.IsNullOrEmpty(appId))
+            {
+                SnsOauthConfigProvider.RegisterDefaultWxConfig(appId,appSecret);
+            }
+        }
+        
+        /// <summary>
+        /// 注册相关插件
+        /// </summary>
+        private static void RegistePlugs()
+        {
+            InsContainer<IEmailPlug>.Set<EmailPlug>();
+            InsContainer<ISmsPlug>.Set<AliSmsPlug>();
+        }
+    }
+}
