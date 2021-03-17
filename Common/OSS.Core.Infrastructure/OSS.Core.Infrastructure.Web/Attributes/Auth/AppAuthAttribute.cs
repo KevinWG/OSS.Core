1d5460e52ff4bcc9c5f8f99be50a002011a54b87
kevin <1985088337@qq.com>
2021-03-17 09:53:22 +0800

    合并文件

--- Common/OSS.Core.Infrastructure/OSS.Core.Infrastructure.Web/Attributes/Auth/AppAuthAttribute.cs
+++ Common/OSS.Core.Infrastructure/OSS.Core.Infrastructure.Web/Attributes/Auth/AppAuthAttribute.cs
@@ -66,4 +66,35 @@ namespace OSS.Core.Infrastructure.Web.Attributes.Auth
         /// </summary>
         public bool IsWebSite { get; set; }
     }
+
+
+    public class AppPartnerNameAttribute : BaseOrderAuthAttribute
+    {
+        private readonly string _appId;
+        private readonly string _appVer;
+
+        /// <summary>
+        ///  专属回调初始化处理过滤器
+        /// </summary>
+        /// <param name="fromApp">来源平台名称</param>
+        /// <param name="fromAppVer"></param>
+        public AppPartnerNameAttribute(string fromApp, string fromAppVer = null)
+        {
+            _appId = string.Concat("Partner_", fromApp);
+            _appVer = fromAppVer ?? string.Empty;
+
+            p_Order = -1001;
+        }
+
+
+        public override Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            var sysInfo = AppWebInfoHelper.InitialDefaultAppIdentity(context.HttpContext);
+
+            sysInfo.app_id = _appId;
+            sysInfo.app_ver = _appVer;
+
+            return Task.CompletedTask;
+        }
+    }
 }
