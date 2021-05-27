9b41d45b549281621c9607377db6e1b49b9dfb4d
kevin <1985088337@qq.com>
2021-05-27 17:12:03 +0800

    优化缓存处理

--- Common/OSS.Core.Infrastructure/OSS.Core.Infrastructure/Extensions/CacheExtension.cs
+++ Common/OSS.Core.Infrastructure/OSS.Core.Infrastructure/Extensions/CacheExtension.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
 using OSS.Common.BasicMos.Resp;
+using OSS.Core.Infrastructure.Helpers;
 using OSS.Tools.Cache;
 
 namespace OSS.Core.Infrastructure.Extensions
 {
-    public static class CacheExtension
+    /// <summary>
+    ///  返回结果(继承Resp)的对象缓存扩展类 
+    /// </summary>
+    public static class RespCacheExtension
     {
         /// <summary>
         ///  根据结果清理缓存
@@ -13,14 +17,34 @@ namespace OSS.Core.Infrastructure.Extensions
         /// <typeparam name="TRes"></typeparam>
         /// <param name="res"></param>
         /// <param name="cacheKey"></param>
+        /// <param name="sourceName"></param>
         /// <returns></returns>
-        public static async Task<TRes> WithCacheClear<TRes>(this Task<TRes> res,params string[] cacheKey)
-            where TRes : Resp
+        public static async Task<TRes> WithCacheClear<TRes>(this Task<TRes> res, string cacheKey, string sourceName = "default")
+            where TRes : Resp, new()
         {
             var r = await res;
             if (r.IsSuccess())
             {
-                await CacheHelper.RemoveAsync(cacheKey);
+                await CacheHelper.RemoveAsync(cacheKey,sourceName);
+            }
+            return r;
+        }
+
+        /// <summary>
+        ///  根据结果清理缓存
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="res"></param>
+        /// <param name="cacheKey"></param>
+        /// <param name="sourceName"></param>
+        /// <returns></returns>
+        public static async Task<TRes> WithCacheClear<TRes>(this Task<TRes> res, string[] cacheKey, string sourceName = "default")
+            where TRes : Resp, new()
+        {
+            var r = await res;
+            if (r.IsSuccess())
+            {
+                await CacheHelper.RemoveAsync(cacheKey, sourceName);
             }
             return r;
         }
@@ -33,15 +57,14 @@ namespace OSS.Core.Infrastructure.Extensions
         /// <param name="cacheKey"></param>
         /// <param name="slidingTime"></param>
         /// <param name="cacheProtectSeconds"></param>
+        /// <param name="sourceName"></param>
         /// <returns></returns>
-        public static Task<TRes> WithCache<TRes>(this Func<Task<TRes>> getFunc, string cacheKey,TimeSpan slidingTime,int cacheProtectSeconds = 10, string sourceName = "default")
-            where TRes : Resp
+        public static Task<TRes> WithCache<TRes>(this Func<Task<TRes>> getFunc, string cacheKey, TimeSpan slidingTime,
+            int cacheProtectSeconds = 10, string sourceName = "default")
+            where TRes : Resp, new()
         {
-            return cacheProtectSeconds==0 
-                ?  CacheHelper.GetOrSetAsync(cacheKey, getFunc, slidingTime, null, sourceName)
-                : WithCache(getFunc, cacheKey, slidingTime, null, cacheProtectSeconds, sourceName);
+          return  RespCacheHelper.GetOrSetAsync(cacheKey, getFunc, slidingTime,null, cacheProtectSeconds, sourceName);
         }
-
 
         /// <summary>
         /// 设置缓存
@@ -49,15 +72,16 @@ namespace OSS.Core.Infrastructure.Extensions
         /// <typeparam name="TRes"></typeparam>
         /// <param name="getFunc"></param>
         /// <param name="cacheKey"></param>
-        /// <param name="absoluteTime"></param>
+        /// <param name="maxAbsoluteTime"></param>
         /// <param name="cacheProtectSeconds"></param>
+        /// <param name="sourceName"></param>
         /// <returns></returns>
-        public static Task<TRes> WithAbsoluteCache<TRes>(this Func<Task<TRes>> getFunc, string cacheKey, TimeSpan absoluteTime, int cacheProtectSeconds = 10, string sourceName = "default")
-            where TRes : Resp
+        public static Task<TRes> WithAbsoluteCache<TRes>(this Func<Task<TRes>> getFunc, string cacheKey,
+            TimeSpan maxAbsoluteTime, int cacheProtectSeconds = 10, string sourceName = "default")
+            where TRes : Resp, new()
         {
-            return cacheProtectSeconds==0 
-                ? CacheHelper.GetOrSetAsync(cacheKey, getFunc,  null, absoluteTime,  sourceName) 
-                : WithCache(getFunc, cacheKey, null, absoluteTime, cacheProtectSeconds, sourceName);
+            return RespCacheHelper.GetOrSetAsync(cacheKey, getFunc, null, maxAbsoluteTime, cacheProtectSeconds,
+                sourceName);
         }
 
 
@@ -72,15 +96,14 @@ namespace OSS.Core.Infrastructure.Extensions
         /// <param name="cacheProtectSeconds"></param>
         /// <param name="sourceName"></param>
         /// <returns></returns>
-        public static Task<TRes> WithCache<TRes>(this Func<Task<TRes>> getFunc, string cacheKey, TimeSpan? slidingTime, TimeSpan? maxAbsoluteTime, int cacheProtectSeconds=10, string sourceName = "default")
-            where TRes : Resp
+        public static Task<TRes> WithCache<TRes>(this Func<Task<TRes>> getFunc, string cacheKey, TimeSpan? slidingTime,
+            TimeSpan? maxAbsoluteTime, int cacheProtectSeconds = 10, string sourceName = "default")
+            where TRes : Resp, new()
         {
-            if (cacheProtectSeconds==0)
-            {
-                return CacheHelper.GetOrSetAsync(cacheKey, getFunc, slidingTime, maxAbsoluteTime, sourceName);
-            }
-            return CacheHelper.GetOrSetAsync(cacheKey, getFunc, slidingTime,maxAbsoluteTime, res => !res.IsSuccess(),
-                cacheProtectSeconds, sourceName);
+            return RespCacheHelper.GetOrSetAsync(cacheKey, getFunc, slidingTime, maxAbsoluteTime, cacheProtectSeconds,
+                sourceName);
         }
+        
+
     }
 }
---
+++ Common/OSS.Core.Infrastructure/OSS.Core.Infrastructure/Helpers/RespCacheHelper.cs
@@ -0,0 +1,125 @@
+﻿using OSS.Common.BasicMos.Resp;
+using OSS.Tools.Cache;
+using System;
+using System.Threading.Tasks;
+
+namespace OSS.Core.Infrastructure.Helpers
+{
+    /// <summary>
+    ///  返回结果(继承Resp)的对象缓存辅助类  
+    /// </summary>
+    public static class RespCacheHelper
+    {
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="getFunc"></param>
+        /// <param name="cacheKey"></param>
+        /// <param name="absoluteTime"></param>
+        /// <param name="cacheProtectSeconds"></param>
+        /// <param name="sourceName"></param>
+        /// <returns></returns>
+        public static Task<TRes> GetOrSetAbsoluteAsync<TRes>(string cacheKey, Func<Task<TRes>> getFunc, 
+            TimeSpan absoluteTime, int cacheProtectSeconds = 10, string sourceName = "default")
+            where TRes : Resp, new()
+        {
+            return GetOrSetAsync(cacheKey, getFunc,  null, absoluteTime, cacheProtectSeconds, sourceName);
+        }
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="getFunc"></param>
+        /// <param name="cacheKey"></param>
+        /// <param name="slidingTime"></param>
+        /// <param name="cacheProtectSeconds"></param>
+        /// <param name="sourceName"></param>
+        /// <returns></returns>
+        public static Task<TRes> GetOrSetAsync<TRes>(string cacheKey, Func<Task<TRes>> getFunc, TimeSpan slidingTime,
+            int cacheProtectSeconds = 10, string sourceName = "default")
+            where TRes : Resp, new()
+        {
+            return GetOrSetAsync(cacheKey, getFunc, slidingTime, null, cacheProtectSeconds, sourceName);
+        }
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="getFunc"></param>
+        /// <param name="cacheKey"></param>
+        /// <param name="slidingTime"></param>
+        /// <param name="maxAbsoluteTime"></param>
+        /// <param name="cacheProtectSeconds"></param>
+        /// <param name="sourceName"></param>
+        /// <returns></returns>
+        public static Task<TRes> GetOrSetAsync<TRes>(string cacheKey, Func<Task<TRes>> getFunc,  TimeSpan? slidingTime,
+            TimeSpan? maxAbsoluteTime, int cacheProtectSeconds = 10, string sourceName = "default")
+            where TRes : Resp, new()
+        {
+            if (cacheProtectSeconds == 0)
+            {
+                return GetOrSetAsync(cacheKey, getFunc, slidingTime, maxAbsoluteTime, sourceName);
+            }
+            return CacheHelper.GetOrSetAsync(cacheKey, getFunc, slidingTime, maxAbsoluteTime, res => !res.IsSuccess(),
+                cacheProtectSeconds, sourceName);
+        }
+
+        /// <summary>
+        ///  根据结果清理缓存
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="cacheKey"></param>
+        /// <param name="sourceName"></param>
+        /// <returns></returns>
+        public static Task<bool> RemoveAsync<TRes>(string cacheKey, string sourceName = "default")
+        {
+            return CacheHelper.RemoveAsync(cacheKey, sourceName);
+        }
+
+        /// <summary>
+        ///  根据结果清理缓存
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="cacheKeys"></param>
+        /// <param name="sourceName"></param>
+        /// <returns></returns>
+        public static Task<bool> RemoveAsync<TRes>(string[] cacheKeys, string sourceName = "default")
+        {
+            return CacheHelper.RemoveAsync(cacheKeys, sourceName);
+        }
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="getFunc"></param>
+        /// <param name="cacheKey"></param>
+        /// <param name="slidingTime"></param>
+        /// <param name="maxAbsoluteTime"></param>
+        /// <param name="sourceName"></param>
+        /// <returns></returns>
+        private static async Task<TRes> GetOrSetAsync<TRes>(string cacheKey, Func<Task<TRes>> getFunc,
+            TimeSpan? slidingTime, TimeSpan? maxAbsoluteTime, string sourceName = "default")
+            where TRes : Resp, new()
+        {
+            var obj = await CacheHelper.GetAsync<TRes>(cacheKey, sourceName);
+            if (obj != null)
+                return obj;
+
+            if (getFunc == null)
+                return new TRes().WithResp(RespTypes.UnKnowOperate, "未实现获取方法！");
+
+            var data = await getFunc.Invoke();
+            if (data == null || !data.IsSuccess())
+                return data;
+
+            await CacheHelper.SetAsync(cacheKey, data, slidingTime, maxAbsoluteTime, sourceName);
+            return data;
+        }
+
+     
+    }
+}
