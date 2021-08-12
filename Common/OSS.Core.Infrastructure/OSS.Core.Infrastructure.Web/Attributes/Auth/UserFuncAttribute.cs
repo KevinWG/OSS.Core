f55286003406f65835ea63a4ad7eab5b5964c1f9
kevin <1985088337@qq.com>
2021-08-12 16:50:20 +0800

    业务权限码处理

--- Common/OSS.Core.Infrastructure/OSS.Core.Infrastructure.Web/Attributes/Auth/UserFuncAttribute.cs
+++ Common/OSS.Core.Infrastructure/OSS.Core.Infrastructure.Web/Attributes/Auth/UserFuncAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Filters;
+using OSS.Common.BasicMos.Resp;
 using OSS.Core.Context;
 
 namespace OSS.Core.Infrastructure.Web.Attributes.Auth
@@ -12,7 +13,7 @@ namespace OSS.Core.Infrastructure.Web.Attributes.Auth
     public class UserFuncAttribute : BaseOrderAuthAttribute
     {
         private readonly string _funcCode;
-        private readonly string _queryCode;
+        private readonly string _queryCodeParaName;
 
         private readonly PortalAuthorizeType _auth_type;
         
@@ -28,8 +29,8 @@ namespace OSS.Core.Infrastructure.Web.Attributes.Auth
         /// 功能权限验证
         /// </summary>
         /// <param name="funcCode"></param>
-        /// <param name="queryCode"></param>
-        public UserFuncAttribute( string funcCode, string queryCode = null):this(PortalAuthorizeType.Admin,funcCode,queryCode)
+        /// <param name="queryCodeParaName">业务 QueryCode 的参数 </param>
+        public UserFuncAttribute( string funcCode, string queryCodeParaName = null):this(PortalAuthorizeType.Admin,funcCode, queryCodeParaName)
         {
         }
 
@@ -38,22 +39,33 @@ namespace OSS.Core.Infrastructure.Web.Attributes.Auth
         /// </summary>
         /// <param name="authType"></param>
         /// <param name="funcCode"></param>
-        /// <param name="queryCode"></param>
-        public UserFuncAttribute(PortalAuthorizeType authType, string funcCode, string queryCode = null)
+        /// <param name="queryCodeParaName">业务 QueryCode 的参数 </param>
+        public UserFuncAttribute(PortalAuthorizeType authType, string funcCode, string queryCodeParaName = null)
         {
             p_Order = -11;
 
             _funcCode  = funcCode;
-            _queryCode = queryCode;
             _auth_type = authType;
+
+            _queryCodeParaName = queryCodeParaName;
         }
 
         /// <inheritdoc />
         public override Task OnAuthorizationAsync(AuthorizationFilterContext context)
         {
-            var appIdentity = CoreAppContext.Identity;
+            var queryCode = string.Empty;
+            if (!string.IsNullOrEmpty(_queryCodeParaName))
+            {
+                queryCode = context.HttpContext.Request.Query[_queryCodeParaName].ToString();
+                if (string.IsNullOrEmpty(queryCode))
+                {
+                    ResponseExceptionEnd(context,new Resp(RespTypes.ParaError,$"请求参数中的权限业务码({_queryCodeParaName})不能为空！"));
+                    return Task.CompletedTask;
+                }
+            }
 
-            appIdentity.ask_func = new AskUserFunc(_auth_type,_funcCode, _queryCode);
+            var appIdentity = CoreAppContext.Identity;
+            appIdentity.ask_func = new AskUserFunc(_auth_type,_funcCode, queryCode);
 
             return Task.CompletedTask;
         }
