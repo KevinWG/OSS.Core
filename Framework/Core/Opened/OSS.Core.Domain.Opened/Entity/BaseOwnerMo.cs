1ebb19a198f9737deb4fffd4312aa8dc7fbbddef
CC <1985088337@qq.com>
2023-05-17 15:57:57 +0800

    优化文件名称

---
+++ Framework/Core/Opened/OSS.Core.Domain.Opened/Entity/BaseOwnerMo.cs
@@ -0,0 +1,29 @@
+﻿namespace OSS.Core.Domain;
+
+/// <summary>
+///  基础所有者实体
+/// </summary>
+/// <typeparam name="IdType"></typeparam>
+public class BaseOwnerMo<IdType> : BaseMo<IdType>
+{
+    /// <summary>
+    ///  【创建/归属】用户Id
+    /// </summary>
+    public long owner_uid { get; set; }
+
+    /// <summary>
+    ///  创建时间
+    /// </summary>
+    public long add_time { get; set; }
+
+}
+
+/// <summary>
+///  对象基类（含创建者Id和通用状态）
+/// </summary>
+/// <typeparam name="IdType"></typeparam>
+public class BaseOwnerAndStateMo<IdType> : BaseOwnerMo<IdType>, IDomainStatus
+{
+    /// <inheritdoc />
+    public CommonStatus status { get; set; }
+}
---
+++ Framework/Core/Opened/OSS.Core.Domain.Opened/Entity/BaseTenantOwnerMo.cs
@@ -0,0 +1,26 @@
+﻿namespace OSS.Core.Domain;
+
+/// <summary>
+///  基础所有者实体（租户）
+/// </summary>
+/// <typeparam name="IdType"></typeparam>
+public class BaseTenantOwnerMo<IdType> : BaseOwnerMo<IdType>, IDomainTenantId<long>
+{
+    /// <summary>
+    ///  租户
+    /// </summary>
+    public long tenant_id { get; set; }
+}
+
+
+/// <summary>
+///  基础所有者实体（租户）
+/// </summary>
+/// <typeparam name="IdType"></typeparam>
+public class BaseTenantOwnerAndStateMo<IdType> : BaseTenantOwnerMo<IdType>
+{
+    /// <summary>
+    /// 状态信息
+    /// </summary>
+    public CommonStatus status { get; set; }
+}
