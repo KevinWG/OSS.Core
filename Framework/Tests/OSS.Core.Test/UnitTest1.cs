5103a4a859cd011bf65145b29a669e72f5da741c
CC <1985088337@qq.com>
2023-05-21 11:01:56 +0800

    添加测试内容

--- Framework/Tests/OSS.Core.Test/UnitTest1.cs
+++ Framework/Tests/OSS.Core.Test/UnitTest1.cs
@@ -18,6 +18,12 @@ namespace OSS.Core.Test
             newIdentity.FormatFromTicket(ticket);
 
             Assert.IsTrue(newIdentity.CheckSign(secret,10, "token").IsSuccess(),"应用签名加密失败!");
+
+            var userIdentity = new UserIdentity
+            {
+                id_type = IdentityType.Admin
+            };
+            Assert.IsTrue(userIdentity.auth_type == AuthorizeType.Admin, "扩展 IdentityType 属性失败!");
         }
     }
 }
