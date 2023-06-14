fd428f86ce843e94b4ed2428c81329ecb744d6af
CC <1985088337@qq.com>
2023-06-14 17:27:43 +0800

    注释

--- Framework/Context/OSS.Core.Context/Mos/AppType.cs
+++ Framework/Context/OSS.Core.Context/Mos/AppType.cs
@@ -7,17 +7,17 @@
 public enum AppType
 {
     /// <summary>
-    ///  平台管理应用
+    ///  平台内部管理应用（多租户）
     /// </summary>
     SystemManager = 1,
 
     /// <summary>
-    ///  平台应用
+    ///  平台内部应用（单租户）
     /// </summary>
     System = 30,
 
     /// <summary>
-    ///  平台上多租户代理应用
+    ///  平台外部多租户代理应用 (多租户)
     /// </summary>
     Proxy = 60,
 
