52eaf11051d078ebd076fd238bc7e8d25cbae2c2
CC <1985088337@qq.com>
2024-04-21 22:59:24 +0800

    添加数字编号生成组件

---
+++ Framework/Component/OSS.Core.Comp.SequenceNum.Mysql/MysqlSequenceNumGenerator.cs
@@ -0,0 +1,14 @@
+﻿using OSS.Tools.Num;
+
+namespace OSS.Core.Comp.SequenceNum;
+
+/// <summary>
+/// mysql 序列数字编号生成实现类
+/// </summary>
+public class MysqlSequenceNumGenerator : ISequenceNumGenerator
+{
+    public Task<(long start, long end)> New(string sequenceKey, int count)
+    {
+        throw new NotImplementedException();
+    }
+}
